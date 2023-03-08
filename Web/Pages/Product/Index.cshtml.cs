b8a1e20a3cc158cdac3ceb179b041ee6c1568943
iBlurBlur <tanakorn.ngam@gmail.com>
2023-03-08 16:00:05 +0700

    Prepare show product list

---
+++ Web/Pages/Product/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Application.Commom.Interfaces;
+using Application.Common.Interfaces;
+using Application.Features.Products.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages.Product
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IProductService _productService;
+
+        public IEnumerable<ProductResponseDTO> Products { get; set; } = Enumerable.Empty<ProductResponseDTO>();
+   
+
+        public IndexModel(IProductService productService)
+        {
+            this._productService = productService;
+        }
+
+        public async Task OnGet()
+        {
+            Products = await _productService.GetProducts();
+        }
+    }
+}
