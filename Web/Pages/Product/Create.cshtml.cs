50d3a3bed06574216d7c572c881fc1dbe3fb676c
iBlurBlur <tanakorn.ngam@gmail.com>
2023-03-09 16:15:52 +0700

    Create product

--- Web/Pages/Product/Create.cshtml.cs
+++ Web/Pages/Product/Create.cshtml.cs
@@ -1,12 +1,90 @@
+using Application.Common.Interfaces;
+using Application.Features.ProductCategories.Models;
+using Application.Features.Products.Models;
+using Mapster;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using Web.ViewModels;
 
 namespace Web.Pages.Product
 {
     public class CreateModel : PageModel
     {
+        private readonly IProductService _productService;
+
+        [BindProperty]
+        public CreateProductViewModel CreateProductViewModel { get; set; } = new CreateProductViewModel();
+
+        public IEnumerable<SelectListItem>? SelectListProductCategories { get; set; }
+
+        public CreateModel(IProductService productService)
+        {
+            this._productService = productService;
+        }
+
         public void OnGet()
         {
+            SetupDropdown();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                SetupDropdown();
+                return Page();
+            }
+            await _productService.AddProduct(CreateProductViewModel.Adapt<CreateProductDTO>());
+            return RedirectToPage("Index");
+        }
+
+        private void SetupDropdown()
+        {
+            List<SelectListItem>? items = GetCategories().Select(
+                c => new SelectListItem() { 
+                    Text = c.Name,
+                    Value = c.ProductCategoryId.ToString()
+                }
+                ).ToList();
+
+            items.Insert(0, new SelectListItem()
+            {
+                Text = "Pick one",
+                Value = string.Empty,
+                Selected = true
+            });
+
+            SelectListProductCategories = items;
+        }
+
+        private IEnumerable<ProductCategoryDTO> GetCategories()
+        {
+            return new List<ProductCategoryDTO>() {
+                                new ProductCategoryDTO
+                                {
+                                    Name = "Bikes",
+                                    ProductCategoryId = 1,
+                                },
+                                new ProductCategoryDTO
+                                {
+                                    Name = "Components",
+                                    ProductCategoryId = 2,
+                                },
+                                new ProductCategoryDTO
+                                {
+                                    Name = "Clothing",
+                                    ProductCategoryId = 3,
+                                },
+                                new ProductCategoryDTO
+                                {
+                                    Name = "Accessories",
+                                    ProductCategoryId = 4,
+                                }
+
+                };
         }
     }
 }
---
+++ Web/ViewModels/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Products.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.ViewModels;
+
+public class ProductViewModel: BaseProductViewModel
+{
+    [Required]
+    public int ProductId { get; set; }
+
+    [HiddenInput]
+    public Guid Rowguid { get; set; }
+
+    public byte[]? ThumbNailPhoto { get; set; }
+
+    public ProductCategoryViewModel? ProductCategory { get; set; }
+}
